64c6764c7532ff79745fb9519ecd0a828cfe7f82
sanavila <rute.leone@hotmail.com>
2022-10-04 17:14:15 -0300

    Requisições get e post

---
+++ js/criarVideo.js
@@ -0,0 +1,7 @@
+const formulario = document.querySelector("[data-formulario");
+
+function criarVideo() {
+    const imagem = document.querySelector("[data-imagem]");
+    const url = document.querySelector("[data-url]");   
+    const titulo = document.querySelector("[data-titulo]");
+}
